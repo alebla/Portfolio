91061aa2ac197274fbe2e2f0c72950f8b749ce11
alebla26 <self.alexb@gmail.com>
2017-09-21 11:46:27 -0500

    Initial Commit

---
+++ Image_Viewer/Image_Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Image_Viewer
+//
+//  Created by Alex Blanchard on 9/21/17.
+//  Copyright © 2017 Alex Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
