5667110dbd647112b7ab8f5c2821d0187d6ba8e0
alebla26 <self.alexb@gmail.com>
2017-09-22 22:38:11 -0500

    Began work on a flag guessing game

---
+++ Guess_Who's_Flag?/Guess_Who's_Flag?/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Guess_Who's_Flag?
+//
+//  Created by Alex Blanchard on 9/22/17.
+//  Copyright © 2017 Alex Blanchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+
+
+}
+
