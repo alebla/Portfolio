91ed451768dc6c8ce5f0bb48032c0efde1bf6a0f
alebla26 <self.alexb@gmail.com>
2017-09-29 00:22:45 -0500

    Attempting to load words file.

--- Anagrams/Anagrams/ViewController.swift
+++ Anagrams/Anagrams/ViewController.swift
@@ -7,20 +7,43 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UITableViewController {
-var words = [String]()
+  var allWords = [String]()
+  var usedWords = [String]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    populateAllWords()
+    startGame()
+  }
+  
+  func populateAllWords() {
+    if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+      if let startWords = try? String(contentsOfFile: startWordsPath) {
+        allWords += startWords.components(separatedBy: "\n")
+        print(startWordsPath)
+      }
+    } else {
+      allWords = ["silkworm"]
+    }
+  }
+  
+  func startGame() {
+    allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+    title = allWords[0]
+    usedWords.removeAll(keepingCapacity: true)
+    tableView.reloadData()
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return words.count
+    return allWords.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
-    
+    cell.textLabel?.text = allWords[indexPath.row]
     return cell
   }
   
