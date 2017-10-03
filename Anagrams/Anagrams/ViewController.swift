cc7e63aa3280fa1e00d16286b7e85883c9a97bb5
alebla26 <self.alexb@gmail.com>
2017-10-03 13:25:12 -0500

    Began intesive Commenting with isPossible(_:). Refactored for readablilty. Left unpolished for now.

--- Anagrams/Anagrams/ViewController.swift
+++ Anagrams/Anagrams/ViewController.swift
@@ -37,11 +37,8 @@ class ViewController: UITableViewController {
   }
   
   func submit(_ answer: String?) {
-    let errorTitle: String
-    let errorMessage: String
-    
     if let answer = answer?.lowercased() {
-      // Disallow words less than 3 letters
+      // Disallow words less than 3 letters.
       if answer.count > 2 {
         if isPossible(answer) {
           if isDuplicate(answer) {
@@ -50,33 +47,20 @@ class ViewController: UITableViewController {
               
               let indexPath = IndexPath(row: 0, section: 0)
               tableView.insertRows(at: [indexPath], with: .automatic)
-              return
-            } else {
-              errorTitle   = "Real Fake Words"
-              errorMessage = "The answer was not a valid word."
-            }
-          } else {
-            errorTitle   = "Duplicate"
-            errorMessage = "You tried that already!"
-          }
-        } else {
-          errorTitle   = "Not an anagram"
-          errorMessage = "Try again. Remeber, letters can only be used once."
-        }
-      } else {
-        errorTitle = "Not Allowed"
-        errorMessage = "Answer must not be shorter than 3 letters"
-      }
-    } else {
-      errorTitle   = "Nil Input"
-      errorMessage = "The input was nil."
-    }
-    
-    let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-    ac.addAction(UIAlertAction(title: "OK", style: .default))
-    present(ac, animated: true)
+              return  /* The following else statements set errors for the user to understand what they did wrong. */
+            } else { showErrorMessage(errorTitle: "Real Fake Words", errorMessage: "The answer was not a valid word.") }
+          } else { showErrorMessage(errorTitle: "Duplicate", errorMessage: "You tried that already!") }
+        } else { showErrorMessage(errorTitle: "Not an anagram", errorMessage: "Try again. Letters can only be used once.") }
+      } else { showErrorMessage(errorTitle: "Not Allowed", errorMessage: "Answer must be 3 letters or more") }
+    } else { showErrorMessage(errorTitle: "Nil Input", errorMessage: "The input was nil.") }
   }
   
+  /** Checks whether or not the users answer contains the letters inside the current word.
+   - Parameters:
+     - answer: Takes a String type
+   - Returns: Always returns a Bool
+   - Throws: Nothing expected
+   */
   func isPossible(_ answer: String) -> Bool {
     var tempWord = title!.lowercased()
     
@@ -90,10 +74,13 @@ class ViewController: UITableViewController {
     return true
   }
   
+  // Checks to see if the user used the answer already.
   func isDuplicate(_ answer: String) -> Bool {
     return !usedWords.contains(answer)
   }
   
+  // Checks to see if the word is real by using the checker variable to see if the word is misspelled. If not the word is real
+  // and the location will be == to the constant NSNotFound.
   func isReal(_ answer: String) -> Bool {
     let checker = UITextChecker()
     let range = NSMakeRange(0, answer.utf16.count)
@@ -101,6 +88,7 @@ class ViewController: UITableViewController {
     return mispelledLocation.location == NSNotFound
   }
   
+  // Loads the words from the imported file into allWords[].
   func populateAllWords() {
     if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
       if let startWords = try? String(contentsOfFile: startWordsPath) {
@@ -111,12 +99,22 @@ class ViewController: UITableViewController {
     }
   }
   
+  // Builds error messages for the users incorrect responses. Responses are set in the calling function.
+  func showErrorMessage(errorTitle: String, errorMessage: String) {
+    let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
+  }
+  
+  // Shuffles allWord[] and displays the current word. Emptys the tableView.
   func startGame() {
     allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
     title = allWords[0]
     usedWords.removeAll(keepingCapacity: true)
     tableView.reloadData()
   }
+  
+  // MARK: TableView Methods
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return usedWords.count
