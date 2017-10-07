7b43301dc7d330329e67cbc31b5e7ffe7f365170
alebla26 <self.alexb@gmail.com>
2017-10-06 23:46:12 -0500

    UI laid out and basic variables and outlets added to the view controller

--- Tricky Little Words/Tricky Little Words/ViewController.swift
+++ Tricky Little Words/Tricky Little Words/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var cluesLabel: UILabel!
+  @IBOutlet weak var answersLabel: UILabel!
+  @IBOutlet weak var currentAnswer: UITextField!
+  @IBOutlet weak var scoreLabel: UILabel!
+  
+  var letterButtons = [UIButton]()
+  var activatedButtons = [UIButton]()
+  var solutions = [String]()
+  
+  var score = 0
+  var level = 1
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  //MARK: IBActions
+  
+  @IBAction func submitTapped(_ sender: UIButton) {
   }
-
-
+  
+  @IBAction func clearTapped(_ sender: UIButton) {
+  }
+  
 }
 
