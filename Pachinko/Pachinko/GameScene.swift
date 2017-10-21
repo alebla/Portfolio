7787676dec8907fab5a5e8b095f714c07bfe1f0f
alebla26 <self.alexb@gmail.com>
2017-10-20 22:57:01 -0500

    reworked for iphone 6s

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -10,7 +10,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "background.jpg")
+        let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
@@ -18,5 +18,14 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            box.position = location
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+            
+            addChild(box)
+        }
     }
 }
