85992dae60ba27c656e9708237437f7d392b9c02
alebla26 <self.alexb@gmail.com>
2017-11-04 21:58:52 -0500

    Began thorough commenting

--- PhotoProcessor/PhotoProcessor/ViewController.swift
+++ PhotoProcessor/PhotoProcessor/ViewController.swift
@@ -11,19 +11,30 @@ import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
+  /// Interface Builder Outlet
+  /** ...
+   - Parameters:
+   */
   @IBOutlet weak var intensity: UISlider!
+  /// Interface Builder Outlet
   @IBOutlet weak var imageView: UIImageView!
-  
+
   var currentImage: UIImage!
   
+  // Stores space for the Core Image Context
   var context: CIContext!
+  
+  // Stores space for the Core Image Filter
   var currentFilter: CIFilter!
+  var filterKeys: [String]!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     buildUI()
   }
   
+  /// Builds the user interface and instantiates variables
   func buildUI() {
     title = "Photo Processor"
     navigationController?.navigationBar.barStyle = .blackTranslucent
@@ -35,6 +46,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     context = CIContext()
     currentFilter = CIFilter(name: "CISepiaTone")
+    
   }
   
   @objc func importPicture() {
@@ -44,7 +56,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     present(picker, animated: true)
   }
   
-  //MARK: Picker Methods
+  //MARK: - Picker Methods
+  
   
   @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
     guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { print("Failed"); return }
@@ -65,7 +78,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   
   //MARK: Filter methods
   
+  /** Uses the context to create a CGImage, applies the filter and sets it to the image window
+   
+   - Parameters: None
+  
+   - Return: void
+   
+   - Throws: No errors are expected
+   
+ */
   func applyProcessing() {
+    //TODO: Needs to check for value key agreement.
     currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
     
     if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
@@ -76,13 +99,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
   }
   
+  /** Sets the User selected Filter
+   - Parameters:
+     - action: a UIAlertAction Object contains filter string
+   - Returns: void
+   - Throws:  No errors expected
+   */
   func setFilter(action: UIAlertAction) {
     guard currentImage != nil else { return }
+    
     currentFilter = CIFilter(name: action.title!)
     currentFilter.setValue(CIImage(image: currentImage), forKey: kCIInputImageKey)
     applyProcessing()
   }
   
+  /** Saves the processed/filtered image
+   - Returns: void
+   - Throws:  No errors expected
+   */
   func saveImage() {
     guard imageView.image != nil else { return }
     UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
@@ -94,10 +128,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   
   //MARK: IBActions
   
+  
   @IBAction func saveImage(_ sender: UIButton) {
     saveImage()
   }
   
+  
+  // Calls setFilter() method for user selection, passes the corresponding UIAlertAction object to the handler
+  // data, returns and then dismisses the .actionSheet.
   @IBAction func changeFilter(_ sender: UIButton) {
     let ac = UIAlertController(title: "Choose Filter", message: nil, preferredStyle: .actionSheet)
     ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
---
+++ PhotoProcessor/PhotoProcessor/aBHelper.swift
@@ -0,0 +1,19 @@
+//
+//  aBHelper.swift
+//  PhotoProcessor
+//
+//  Created by Alex Blanchard on 11/4/17.
+//  Copyright © 2017 Alex Blanchard. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: Comments
+
+///FUNCTION DECLARATION
+/** ...
+ - Parameters:
+ - Returns: void
+ - Throws:  No errors expected
+ */
